618c6f45b25c572f6aeb22c2acafda861556d1b6
TranMinhTam <tamdc21v7n561@vlvh.ctu.edu.vn>
2025-10-01 15:24:40 +0700

    Hien thi danh sach Contact lay ve tu server

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  props: {
+    contact: { type: Object, required: true },
+  },
+    emits: ["toggle-favorite"],
+  methods: {
+    toggleFavorite(event) {
+      const checked = event.target.checked;
+      this.$emit("toggle-favorite", { ...this.contact, favorite: checked });
+    },
+  },
+};
+</script>
+
+<template>
+  <div>    
+    <div class="p-1">
+      <strong>Tên:</strong> {{ contact.name }}
+    </div>
+    <div class="p-1">
+      <strong>E-mail:</strong> {{ contact.email }}
+    </div>
+    <div class="p-1">
+      <strong>Địa chỉ:</strong> {{ contact.address }}
+    </div>
+    <div class="p-1">
+      <strong>Điện thoại:</strong> {{ contact.phone }}
+    </div>
+    <div class="p-1">
+      <strong>Liên hệ yêu thích:&nbsp;</strong>
+      <i v-if="contact.favorite" class="fas fa-check text-success"></i>
+      <i v-else class="fas fa-times text-danger"></i>
+    </div>
+  </div>
+</template>
