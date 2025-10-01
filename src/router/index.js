f2d1a7eda07c89a5458c2cf283c16b314ea8327e
TranMinhTam <tamdc21v7n561@vlvh.ctu.edu.vn>
2025-10-01 15:36:46 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,14 @@ const routes = [
         path: "/",
         name: "contactbook",
         component: ContactBook,
-    }
-];
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 
+];
 const router  = createRouter({
     history:createWebHistory(import.meta.env.BASE_URL),
     routes,
